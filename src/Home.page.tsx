2ce17b7853489878ac74ad880927b9dcabee5505
sompakorn-lap <sompakorn.lap@student.mahidol.edu>
2024-12-04 15:25:12 +0700

    fixed: remove unuse code

--- src/Home.page.tsx
+++ src/Home.page.tsx
@@ -1,10 +1,4 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-
 function Home() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="p-2">
       <h1 className="text-xl text-teal-400">Home</h1>
