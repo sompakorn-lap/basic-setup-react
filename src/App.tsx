7374bbf2f80617897ade07ec82a838bd6ec024ba
sompakorn-lap <sompakorn.lap@student.mahidol.edu>
2024-12-06 21:06:36 +0700

    feat: change default style

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Icon } from '@iconify/react';
 import Home from "./Home.page";
 
 function App() {
   return (
-    <div className="grid grid-rows-[auto_1fr_auto] min-h-screen p-2">
+    <div className="grid grid-rows-[auto_1fr_auto] min-h-screen">
       
-      <header>
+      <header className="w-full p-4 text-xl text-center bg-black text-gray-100">
         <h1>BomSLap - React</h1>
       </header>
 
-      <main>
+      <main className="p-2">
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Home/>} />
@@ -17,8 +18,14 @@ function App() {
         </BrowserRouter>
       </main>
 
-      <footer>
-        footer
+      <footer className="w-full p-2 bg-black text-gray-100">
+        <a
+          className="flex gap-2"
+          href="https://github.com/sompakorn-lap/basic-setup-react"
+        >
+          <Icon icon="mdi:github" width={24} height={24}/>
+          <span>GITHUB</span>
+        </a>
       </footer>
 
     </div>
